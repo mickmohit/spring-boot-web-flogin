749c33543e8a26578a9fb753477c8735fbd10b93
Mohit Darmwal <36398566+mickmohit@users.noreply.github.com>
2018-04-22 22:03:44 +0530

    Add files via upload

---
+++ spring-boot-web-flogin/src/main/java/com/example/springbootwebflogin/SpringBootWebFloginApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootwebflogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebFloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebFloginApplication.class, args);
+	}
+}
---
+++ spring-boot-web-flogin/src/test/java/com/example/springbootwebflogin/SpringBootWebFloginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springbootwebflogin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWebFloginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
